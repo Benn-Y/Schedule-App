86e7328333a69151f8c95d7a2562199f18df1f6f
Benny Blinks <88045367+BennyBlinks10@users.noreply.github.com>
2022-08-15 17:38:14 +0000

    Add files via upload

--- main/src/main/java/com/example/calendar/MainActivity.java
+++ main/src/main/java/com/example/calendar/MainActivity.java
@@ -1,269 +1,132 @@
-package com.example.calendar;
+package com.example.alarm;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.SearchView;
 
-import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
-import android.provider.CalendarContract;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.renderscript.ScriptGroup;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.CalendarView;
-import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.Toast;
+import com.example.alarm.databinding.ActivityMainBinding;
+import com.google.android.material.timepicker.MaterialTimePicker;
+import com.google.android.material.timepicker.TimeFormat;
 
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-//import com.example.addlist.NotesTakerActivity;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-
+import java.nio.channels.Channel;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
-    FloatingActionButton fab, fab1, fab2, fab3;
-    Animation fabOpen, fabClose, rotateForward, rotateBackward;
-    boolean isOpen = false;
-    Button allReminders;
-
-
-    ListView listView;
-    String[] reminders = {"Eric", "Ama", "Mensah", "Sandra", "Abigail", "Stack", "Stacy"};
-    // will insert this in the reminders page.
-    ArrayAdapter<String> arrayAdapter;
-    CalendarView calendarView;
-    TextView myDate;
+    private ActivityMainBinding binding;
+    private MaterialTimePicker picker;
+    private Calendar calendar;
+    private AlarmManager alarmManager;
+    private PendingIntent pendingIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        setTitle("Home");
 
-        allReminders = (Button) findViewById(R.id.reminders);
-        fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab1 = (FloatingActionButton) findViewById(R.id.fab1);
-        fab2 = (FloatingActionButton) findViewById(R.id.fab2);
-        fab3 = (FloatingActionButton) findViewById(R.id.fab_add);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        fabOpen = AnimationUtils.loadAnimation(this, R.anim.fab_open);
-        fabClose = AnimationUtils.loadAnimation(this, R.anim.fab_close);
+        createNotificationChannel();
 
-        rotateForward = AnimationUtils.loadAnimation(this, R.anim.rotate_forward);
-        rotateBackward = AnimationUtils.loadAnimation(this, R.anim.rotate_backward);
-
-
-        fab.setOnClickListener(new View.OnClickListener() {
+        binding.selectTime.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                animateFab();
+            public void onClick(View v) {
+                showTimePicker();
 
             }
+        });
+        binding.alarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setAlarmPicker();
 
-            private void animateFab() {
-                if (isOpen) {
-                    fab.startAnimation(rotateForward);
-                    fab1.startAnimation(fabClose);
-                    fab2.startAnimation(fabClose);
-                    fab3.startAnimation(fabClose);
-                    fab1.setClickable(false);
-                    fab2.setClickable(false);
-                    fab3.setClickable(false);
-                    isOpen = false;
-                } else {
-                    fab.startAnimation(rotateBackward);
-                    fab1.startAnimation(fabOpen);
-                    fab2.startAnimation(fabOpen);
-                    fab3.startAnimation(fabOpen);
-                    fab1.setClickable(true);
-                    fab2.setClickable(true);
-                    fab3.setClickable(true);
-                    isOpen = true;
+            }
+        });
+        binding.cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                cancelAlarm();
+
+            }
+        });
+    }
+
+    private void cancelAlarm() {
+        Intent i = new Intent(this,AlarmReceiver.class);
+
+        pendingIntent = PendingIntent.getBroadcast(this,0,i,0);
+
+        if (alarmManager == null){
+            alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        }
+        alarmManager.cancel(pendingIntent);
+        Toast.makeText(this,"Alarm Cancelled", Toast.LENGTH_LONG).show();
+    }
+
+    private void setAlarmPicker() {
+
+        alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        Intent i = new Intent(this,AlarmReceiver.class);
+
+        pendingIntent = PendingIntent.getBroadcast(this,0,i,0);
+
+        alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP,calendar.getTimeInMillis(),
+                AlarmManager.INTERVAL_DAY,pendingIntent);
+
+        Toast.makeText(this,"Alarm set Successfully",Toast.LENGTH_LONG).show();
+    }
+
+    private void showTimePicker() {
+         picker = new MaterialTimePicker.Builder()
+                .setTimeFormat(TimeFormat.CLOCK_12H)
+                .setHour(12)
+                .setMinute(0)
+                .setTitleText("select  Time")
+                .build();
+
+        picker.show(getSupportFragmentManager(),"Alarm");
+        picker.addOnPositiveButtonClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (picker.getHour() > 12){
+                    binding.select.setText(String.format("%02d",(picker.getHour()-12))+" : " + String.format("%02d",(picker.getMinute())) + "PM"
+                    );
                 }
-            }
-        });
-        fab.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View view) {
-                Toast.makeText(getApplicationContext(), "Add New Activities", Toast.LENGTH_LONG).show();
-                return true;
-            }
-        });
-
-        fab1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, CreateReminderActivity.class));
+                else {
+                    binding.select.setText(picker.getHour()+ " : "+ picker.getMinute() + "AM" );
+                }
+                calendar = Calendar.getInstance();
+                calendar.set(Calendar.HOUR_OF_DAY, picker.getHour());
+                calendar.set(Calendar.MINUTE, picker.getMinute());
+                calendar.set(Calendar.SECOND,0);
+                calendar.set(Calendar.MILLISECOND,0);
 
             }
         });
-
-        fab1.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View view) {
-                Toast.makeText(getApplicationContext(), "Add Reminder", Toast.LENGTH_LONG).show();
-                return true;
-            }
-        });
-
-        fab2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, AlarmActivity.class));
-
-            }
-        });
-
-        fab2.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View view) {
-                Toast.makeText(getApplicationContext(), "Add An Alarm", Toast.LENGTH_LONG).show();
-                return true;
-            }
-        });
-        fab3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, NoteListenerActivity.class));
-
-            }
-        });
-
-        fab3.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View view) {
-                Toast.makeText(getApplicationContext(), "Add Note", Toast.LENGTH_LONG).show();
-                return true;
-            }
-        });
-
-
-        calendarView = (CalendarView) findViewById(R.id.calendarView);
-        myDate = (TextView) findViewById(R.id.textView);
-
-        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-            @Override
-            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
-                String date = (i1 + 1) + "/" + i2 + "/" + i;
-                myDate.setText(date);
-            }
-
-
-        });
-        allReminders.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View view) {
-                Toast.makeText(MainActivity.this, "View All Reminders", Toast.LENGTH_LONG).show();
-                return true;
-            }
-        });
-
-        listView = findViewById(R.id.listView);
-        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, reminders);
-
-        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-            @Override
-            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
-
-            }
-        });
-
-
-
-
     }
-    public void openRemindersActivity(View view){
-        Intent intent=new Intent(this, RemindersActivity.class);
-        startActivity(intent);
-    }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu, menu);
-        MenuItem menuItem = menu.findItem(R.id.action_search);
-        SearchView searchView = (SearchView) menuItem.getActionView();
-        searchView.setQueryHint("Search for Reminders");
 
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                return false;
-            }
-        });
-           /*Override
-            public boolean onQueryTextSubmit(String query) {
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                return false;
-            }
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                arrayAdapter.getFilter().filter(newText);
-                return false;
-            }*/
-       // });
-
-
-
-
-
-
-    return super.onCreateOptionsMenu(menu);
-
-    };
-
-
-
-
-
+    private void createNotificationChannel() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            CharSequence name = "Alarm";
+            String description = "Alarm Reminder";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel channel = new NotificationChannel("Alarm",name,importance);
+            channel.setDescription(description);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
